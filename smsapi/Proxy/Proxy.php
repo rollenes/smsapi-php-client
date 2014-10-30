76f47eea932249e27c6aa99bd0c98b5115024ab6
rollen <rafal.wartalski@gmail.com>
2014-10-31 00:35:42 +0100

    Proxy getters marked as deprecated

--- smsapi/Proxy/Proxy.php
+++ smsapi/Proxy/Proxy.php
@@ -8,13 +8,23 @@ interface Proxy
 {
     public function execute(AbstractAction $action);
 
+    /**
+     * @deprecated since version 1.2
+     */
     public function getProtocol();
 
+    /**
+     * @deprecated since version 1.2
+     */
     public function getHost();
 
+    /**
+     * @deprecated since version 1.2
+     */
     public function getPort();
 
     /**
+     *
      * @return Uri
      */
     public function createUri($query, $path);
